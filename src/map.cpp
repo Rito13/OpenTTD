b3b3cbdbc1571586e572220fd054a74ae9afe155
Michael Lutz <michi@icosahedron.de>
2024-11-16 16:28:01 +0100

    Codechange: Fold TileExtended into TileBase.

--- src/map.cpp
+++ src/map.cpp
@@ -24,7 +24,6 @@
 /* static */ uint Map::tile_mask; ///< _map_size - 1 (to mask the mapsize)
 
 /* static */ std::unique_ptr<Tile::TileBase[]> Tile::base_tiles; ///< Base tiles of the map
-/* static */ std::unique_ptr<Tile::TileExtended[]> Tile::extended_tiles; ///< Extended tiles of the map
 
 
 /**
@@ -53,7 +52,6 @@
 	Map::tile_mask = Map::size - 1;
 
 	Tile::base_tiles = std::make_unique<Tile::TileBase[]>(Map::size);
-	Tile::extended_tiles = std::make_unique<Tile::TileExtended[]>(Map::size);
 
 	AllocateWaterRegions();
 }
