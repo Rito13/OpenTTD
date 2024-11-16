3c5faa93a5b332ece408402695355fb6305fe8ca
Michael Lutz <michi@icosahedron.de>
2024-11-16 22:47:30 +0100

    Add: Extend the land info window to display information about all associated tiles

--- src/clear_cmd.cpp
+++ src/clear_cmd.cpp
@@ -458,7 +458,7 @@ void GenerateClearTile()
 }
 
 /** @copydoc GetTileDescProc */
-static void GetTileDesc_Clear(TileIndex tile, TileDesc &td)
+static void GetTileDesc_Clear([[maybe_unused]] TileIndex index, Tile tile, TileDesc &td)
 {
 	/* Each pair holds a normal and a snowy ClearGround description. */
 	static constexpr EnumIndexArray<std::pair<StringID, StringID>, ClearGround, ClearGround::MaxSize> clear_land_str{{{
