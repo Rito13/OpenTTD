62dd0cfc72cd8b59ba4f7138c215b36908dbe778
Cyprian Klimaszewski <kapitan.cypek@gmail.com>
2026-09-14 10:27:17 +0200

    Codefix: Silence some release build warnings

--- src/tunnelbridge_cmd.cpp
+++ src/tunnelbridge_cmd.cpp
@@ -1626,7 +1626,7 @@ void DrawBridgeMiddle(const TileInfo *ti, BridgePillarFlags blocked_pillars)
 	TransportType transport_type = GetTunnelBridgeTransportType(rampsouth);
 	Axis axis = GetBridgeAxis(ti->tile);
 	BridgePillarFlags pillars;
-	bool is_custom_layout; // Set if rail/road bridge uses a custom layout.
+	bool is_custom_layout = false; // Set if rail/road bridge uses a custom layout.
 
 	uint base_offset = GetBridgeMiddleAxisBaseOffset(axis);
 	std::span<const PalSpriteID> psid;
